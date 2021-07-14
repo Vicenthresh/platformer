66c615dc349e7d14df67156febc7be518748d5fe
Vicenthresh <vsolis199@gmail.com>
2021-07-14 18:44:15 -0400

    player and enemy collision fix, wall jump testing

--- src/Actors/Enemy.gd
+++ src/Actors/Enemy.gd
@@ -1,5 +1,7 @@
 extends "res://src/Actors/Actor.gd"
 
+onready var collision_shape_2d: = $CollisionShape2D
+
 func _ready():
 	set_physics_process(false)
 	_velocity.x = -speed.x
@@ -7,12 +9,14 @@ func _ready():
 func _on_StompDetector_body_entered(body):
 	if body.global_position.y > get_node("StompDetector").global_position.y:
 		return
-	get_node("CollisionShape2D").disabled = true
+	
+	hide()
+	collision_shape_2d.set_deferred("disabled", true)
 	queue_free()
 	
 func _physics_process(delta):
 	_velocity.y += gravity * delta
 	
 	if is_on_wall():
-		_velocity *= -1.0
+		_velocity.x *= -1.0
 	_velocity.y = move_and_slide(_velocity, FLOOR_NORMAL).y
--- src/Actors/Player.gd
+++ src/Actors/Player.gd
@@ -3,17 +3,23 @@ extends Actor
 export var stomp_impulse: = 1000.0
 
 onready var animation_player: = $AnimationPlayer
-onready var sprite = $player
+onready var sprite: = $player
+onready var collision_shape_2d: = $CollisionShape2D
+
+var wall_jump_velocity: = Vector2(-5.0, -1.0)
 
 func _on_EnemyDetector_area_entered(area):
 	_velocity = calculate_stomp_velocity(_velocity, stomp_impulse)
 
 func _on_EnemyDetector_body_entered(body):
+	hide()  # Player disappears after being hit.
+	collision_shape_2d.set_deferred("disabled", true)
 	queue_free()
 	
 func _physics_process(delta):
 	var is_jump_interrupted: = Input.is_action_just_released("jump") and _velocity.y < 0.0
 	var direction: = get_direction()
+	
 	_velocity = calculate_move_velocity(_velocity, direction, speed, is_jump_interrupted)
 	_velocity = move_and_slide(_velocity, FLOOR_NORMAL)
 	
@@ -27,11 +33,19 @@ func _physics_process(delta):
 	
 
 func get_direction() -> Vector2:
-	var direction: = Vector2(
-		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
-		-1.0 if Input.is_action_just_pressed("jump") and is_on_floor() else 0.0
-	)
-		
+	var direction: = Vector2.ZERO
+	
+	direction.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	
+	if Input.is_action_just_pressed("jump"):
+		if is_on_floor():
+			direction.y = -1.0
+		elif is_on_wall():
+			direction.x *= wall_jump_velocity.x 
+			direction.y = wall_jump_velocity.y
+		else:
+			direction.y = 0.0
+		   
 	return direction
 
 func calculate_move_velocity(	
@@ -47,8 +61,13 @@ func calculate_move_velocity(
 			new_velocity.y = speed.y * direction.y
 		elif direction.y == 1.0:
 			new_velocity.x *= 0.7
+			
 		if is_jump_interrupted:
 			new_velocity.y = 0.0
+		
+		if is_on_wall():
+			var max_velocity = 200
+			new_velocity.y = min(new_velocity.y, max_velocity)
 			
 		return new_velocity
 
