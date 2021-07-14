086a1e944fb35c67088c655e6529e1c03b239d04
Vicenthresh <vsolis199@gmail.com>
2021-07-13 22:22:54 -0400

    enemies, player camera and level

--- src/Actors/Player.gd
+++ src/Actors/Player.gd
@@ -1,5 +1,13 @@
 extends Actor
 
+export var stomp_impulse: = 1000.0
+
+func _on_EnemyDetector_area_entered(area):
+	_velocity = calculate_stomp_velocity(_velocity, stomp_impulse)
+
+func _on_EnemyDetector_body_entered(body):
+	queue_free()
+	
 func _physics_process(delta):
 	var is_jump_interrupted: = Input.is_action_just_released("jump") and _velocity.y < 0.0
 	var direction: = get_direction()
@@ -9,7 +17,7 @@ func _physics_process(delta):
 func get_direction() -> Vector2:
 	return  Vector2(
 		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
-		-1.0 if Input.is_action_just_pressed("jump") and is_on_floor() else 1.0
+		-1.0 if Input.is_action_just_pressed("jump") and is_on_floor() else 0.0
 	)
 
 func calculate_move_velocity(	
@@ -24,8 +32,13 @@ func calculate_move_velocity(
 		if direction.y == -1.0:
 			new_velocity.y = speed.y * direction.y
 		if not is_on_floor() and direction.y == 1.0:
-			new_velocity.x = new_velocity.x * 0.7
+			new_velocity.x *= 0.7
 		if is_jump_interrupted:
 			new_velocity.y = 0.0
 			
 		return new_velocity
+
+func calculate_stomp_velocity(linear_velocity: Vector2, impulse: float) -> Vector2:
+	var new_velocity: = linear_velocity
+	new_velocity.y = -impulse
+	return new_velocity
